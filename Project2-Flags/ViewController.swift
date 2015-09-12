7f0242605217ce39a7265d308118df3f893f1c88
Robert McBryde <robert.mcbryde@kotikan.com>
2015-09-12 14:47:52 +0100

    Addition of setup of a string of the countries we are playing with and askQuestion() function to populate each button with an image

--- Project2-Flags/ViewController.swift
+++ Project2-Flags/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    // MARK: initialzation 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        countries += ["estonia","france","germany","iceland","italy","monaco","nigeria","poland","russia","spain","uk","us"]
+        askQuestion()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: Custom functions
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+    }
 
 }
 
