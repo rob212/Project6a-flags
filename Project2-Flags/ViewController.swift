2d119af0bb8de7de440390359a503d1a323db214
Robert McBryde <robert.mcbryde@kotikan.com>
2015-09-12 19:14:54 +0100

    fixed nil image issue

--- Project2-Flags/ViewController.swift
+++ Project2-Flags/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        countries += ["estonia","france","germany","iceland","italy","monaco","nigeria","poland","russia","spain","united kingom","united states"]
+        countries += ["estonia","france","germany","ireland","italy","monaco","nigeria","poland","russia","spain","uk","us"]
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
